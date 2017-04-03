8646c38ac4f53ac5890db4c2db9af26fc0e77b70
Arjun P A <arjun.pa2@gmail.com>
2017-04-03 11:26:39 +0530

    Initial Commit

---
+++ PodcastPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodcastPlayer
+//
+//  Created by Arjun P A on 03/04/17.
+//  Copyright © 2017 Arjun P A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
